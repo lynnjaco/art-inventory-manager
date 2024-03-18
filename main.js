8797c535d27e361b145f21923b63a669065fd759
lynnjaco <jacobs.lynnette@gmail.com>
2024-03-18 16:57:35 -0400

    replaces table inventory listing style with a product inventory listing style

---
+++ main.js
@@ -0,0 +1,18 @@
+/* create variables for 
+    product name - id = 
+    product image
+    product description
+    base price
+    date added
+
+    options
+        canvas
+            small       
+            medium      
+            large       
+        photo
+            small       
+            medium    
+            large
+        original
+            original
