3d93bd4f63c86db93790a76f2807cf3e2ade63d5
lynnjaco <jacobs.lynnette@gmail.com>
2024-03-19 03:29:41 -0400

    template appends to the inventory container, but preview does not yet clear, thus new card cannot be generated.  also, card template styling needs adjusting - new product cards do not match

--- main.js
+++ main.js
@@ -1,33 +1,6 @@
 // ADD NEW PRODUCT FORM
 const addProductForm = document.querySelector("#add-product-form");
 
-// form inputs
-const productName = addProductForm.productName;
-const productImage = addProductForm.productImage;
-const basePrice = addProductForm.addProductForm;
-
-// option selections
-const canvasOptions = document.querySelector("#canvas-sizes");
-const canvasCheck = document.querySelector("#canvasOptionCheck");
-const canvasSelection = document.querySelector("#canvas-selction");
-const smallCanvasOption = addProductForm.canvasSmall;
-const mediumCanvasOption = addProductForm.canvasMedium;
-const largeCanvasOption = addProductForm.canvasLarge;
-
-const originalEditionOption = addProductForm.theOriginal;
-const originalCheck = document.querySelector("#originalOptionCheck");
-
-const photoOptions = document.querySelector("#photo-sizes");
-const photoCheck = document.querySelector("#photoOptionCheck");
-const smallPhotoOption = addProductForm.photoSmall;
-const mediumPhotoOption = addProductForm.photoMedium;
-const largePhotoOption = addProductForm.photoLarge;
-
-const productDescription = addProductForm.productDescription;
-
-// form submit button
-const submitButton = document.querySelector("#add-product-button");
-
 // NEW PRODUCT PREVIEW
 const previewListingName = document.querySelector("#listing-name");
 const previewProductImage = document.querySelector("#image-preview");
@@ -51,27 +24,36 @@ const largePhotoPreview = document.querySelector("#lp-preview");
 
 // TOTAL INVENTORY VALUE
 const totalInventoryValue = document.querySelector("#total-value");
-const productInventoryTemplate = document.querySelector("#product-inventory-div").content;
-const productTemplateCopy = document.importNode(productInventoryTemplate, true);
 
-// live transferring form data to preview data
+// FORM INPUTS AND EVENTS
+
+// transfers input values to product specification preview box
+const productName = addProductForm.productName;
 productName.addEventListener('input', (e) => {
     previewListingName.innerHTML = `"${e.target.value}"`;
     e.preventDefault();
 }) 
 
+const productImage = addProductForm.productImage;
 productImage.addEventListener('input', (e) => {
     let url = URL.createObjectURL(e.target.files[0]);
     previewProductImage.style.backgroundImage = "url(" + url + ")";
     e.preventDefault();
 }) 
 
+const productDescription = addProductForm.productDescription;
 productDescription.addEventListener('input', (e) => {
     previewProductDesc.innerHTML = e.target.value;
     e.preventDefault();
-}) 
+})
+
+const basePrice = addProductForm.addProductForm;
+
+// product option selections
 
 // engage product option checkbox to display inventory inputs and preview
+const canvasOptions = document.querySelector("#canvas-sizes");
+const canvasCheck = document.querySelector("#canvasOptionCheck");
 canvasCheck.addEventListener("input", (e) => {
     canvasOptionCheck();
     e.preventDefault();
@@ -86,7 +68,9 @@ function canvasOptionCheck() {
     }
 }
 
-photoCheck.addEventListener("input", () => {
+const photoOptions = document.querySelector("#photo-sizes");
+const photoCheck = document.querySelector("#photoOptionCheck");
+photoCheck.addEventListener("input", (e) => {
     photoOptionCheck();
     e.preventDefault();
 })
@@ -100,7 +84,10 @@ function photoOptionCheck() {
     }
 }
 
-originalCheck.addEventListener("input", () => {
+
+const originalEditionOption = addProductForm.theOriginal;
+const originalCheck = document.querySelector("#originalOptionCheck");
+originalCheck.addEventListener("input", (e) => {
     originalOptionCheck();
     e.preventDefault();
 })
@@ -112,24 +99,12 @@ function originalOptionCheck() {
     }
 }
 
-
-// transfer inventory amount inputs to the preview screen
-originalEditionOption.addEventListener("input", (e) => {
-    let ogVal = originalPreview.value;
-    if(originalEditionOption.checked) {
-        ogVal = `$${basePrice.value * 110}`;
-        originalPreview.innerText = `${ogVal}`;
-        originalPreview.style.display = "flex";
-    } else {
-        ogVal = 0;
-        originalPreview.innerText = "";
-        originalPreview.style.display = "none";
-    } 
-    e.preventDefault();
-})
-
+const smallCanvasOption = addProductForm.canvasSmall;
+const mediumCanvasOption = addProductForm.canvasMedium;
+const largeCanvasOption = addProductForm.canvasLarge;
+// displays canvas inventory inputs and ppus to preview
+let scVal = smallCanvasPreview.value;
 smallCanvasOption.addEventListener("input", (e) => {
-    let scVal = smallCanvasPreview.value;
     if(e.target.value > 0) {
         scVal = basePrice.value * 20;
         smallCanvasPreview.textContent = `12 x 16 | $${scVal}`;
@@ -141,9 +116,8 @@ smallCanvasOption.addEventListener("input", (e) => {
     }
     e.preventDefault();
 })
-
+let mcVal = mediumCanvasPreview.value;
 mediumCanvasOption.addEventListener("input", (e) => {
-    let mcVal = mediumCanvasPreview.value;
     if(e.target.value > 0) {
         mcVal = basePrice.value * 40;
         mediumCanvasPreview.textContent = `24 x 32 | $${mcVal}`;
@@ -155,9 +129,8 @@ mediumCanvasOption.addEventListener("input", (e) => {
     }
     e.preventDefault();
 })
-
+let lcVal = largeCanvasPreview.value;
 largeCanvasOption.addEventListener("input", (e) => {
-    let lcVal = largeCanvasPreview.value;
     if(e.target.value > 0) {
         lcVal = basePrice.value * 60;
         largeCanvasPreview.textContent = `36 x 48 | $${lcVal}`;
@@ -170,11 +143,27 @@ largeCanvasOption.addEventListener("input", (e) => {
     e.preventDefault();
 })
 
+// displays original product price in preview
+let ogVal = originalPreview.value;
+originalEditionOption.addEventListener("input", (e) => {
+    if(originalEditionOption.checked) {
+        ogVal = `$${basePrice.value * 110}`;
+        originalPreview.innerText = `${ogVal}`;
+        originalPreview.style.display = "flex";
+    } else {
+        ogVal = 0;
+        originalPreview.innerText = "";
+        originalPreview.style.display = "none";
+    } 
+    e.preventDefault();
+})
 
-
-
+const smallPhotoOption = addProductForm.photoSmall;
+const mediumPhotoOption = addProductForm.photoMedium;
+const largePhotoOption = addProductForm.photoLarge;
+// displays photo inventory inputs and ppus to preview
+let spVal = smallPhotoPreview.value;
 smallPhotoOption.addEventListener("input", (e) => {
-    let spVal = smallPhotoPreview.value;
     if(e.target.value > 0) {
         spVal = basePrice.value * 10;
         smallPhotoPreview.textContent = `12 x 16 | $${spVal}`;
@@ -186,9 +175,8 @@ smallPhotoOption.addEventListener("input", (e) => {
     }
     e.preventDefault();
 })
-
+let mpVal = mediumPhotoPreview.value;
 mediumPhotoOption.addEventListener("input", (e) => {
-    let mpVal = mediumPhotoPreview.value;
     if(e.target.value > 0) {
         mpVal = basePrice.value * 20;
         mediumPhotoPreview.textContent = `24 x 32 | $${mpVal}`;
@@ -200,9 +188,8 @@ mediumPhotoOption.addEventListener("input", (e) => {
     }
     e.preventDefault();
 })
-
+let lpVal = largePhotoPreview.value;
 largePhotoOption.addEventListener("input", (e) => {
-    let lpVal = largePhotoPreview.value;
     if(e.target.value > 0) {
         lpVal = basePrice.value * 30;
         largePhotoPreview.textContent = `36 x 48 | $${lpVal}`;
@@ -215,6 +202,78 @@ largePhotoOption.addEventListener("input", (e) => {
     e.preventDefault();
 })
 
-// calculate total inventory value
+// form submit button
+const inventoryContainer = document.querySelector("#inventory-container");
+const submitButton = document.querySelector("#add-product-button");
 
-// add product button > adds product inventory card
+const productInventoryTemplate = document.querySelector("#product-inventory-div").content;
+const productTemplateCopy = document.importNode(productInventoryTemplate, true);
+
+submitButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    productTemplateCopy.querySelector("#ic-product-name").textContent = productName.value;
+    // productTemplateCopy.querySelector("#date-added").textContent = new Date();
+    
+    productTemplateCopy.querySelector("#ic-image-preview").style.backgroundImage = "url(" + URL.createObjectURL(productImage.files[0]) + ")";
+    productTemplateCopy.querySelector("#ic-product-description").textContent = productDescription.value;
+    
+    productTemplateCopy.querySelector("#ic-small-canvas-quantity").textContent = smallCanvasOption.value;
+    productTemplateCopy.querySelector("#ic-sc-ppu").textContent = `$${scVal}`;
+
+    productTemplateCopy.querySelector("#ic-medium-canvas-quantity").textContent = mediumCanvasOption.value;
+    productTemplateCopy.querySelector("#ic-mc-ppu").textContent = `$${mcVal}`;
+
+    productTemplateCopy.querySelector("#ic-large-canvas-quantity").textContent = largeCanvasOption.value;
+    productTemplateCopy.querySelector("#ic-lc-ppu").textContent = `$${lcVal}`;
+
+    productTemplateCopy.querySelector("#ic-og-price").textContent = `$${ogVal}`;
+    originalOptionCheck.checked === true ? productTemplateCopy.querySelector("#og-quantity").textContent = "1" : "0";
+
+    productTemplateCopy.querySelector("#ic-small-photo-quantity").textContent = smallPhotoOption.value;
+    productTemplateCopy.querySelector("#ic-sp-ppu").textContent = `$${spVal}`;
+
+    productTemplateCopy.querySelector("#ic-medium-photo-quantity").textContent = mediumPhotoOption.value;
+    productTemplateCopy.querySelector("#ic-mp-ppu").textContent = `$${mpVal}`;
+
+    productTemplateCopy.querySelector("#ic-large-photo-quantity").textContent = largePhotoOption.value;
+    productTemplateCopy.querySelector("#ic-lp-ppu").textContent = `$${lpVal}`;
+    inventoryContainer.append(productTemplateCopy);
+    addProductForm.reset();
+    console.log(productTemplateCopy);
+})
+
+// function populateProductTemplate(productTemplateCopy){
+//     cardTemplate.querySelector("#ic-product-name").textContent = productName.value;
+//     cardTemplate.querySelector("#date-added").textContent = new Date();
+
+//     cardTemplate.querySelector("#ic-image-preview").style.backgroundImage = "url(" + URL.createObjectURL(productImage.target.files[0]) + ")";
+//     cardTemplate.querySelector("#ic-product-description").textContent = productDescription.value;
+    
+//     cardTemplate.querySelector("#ic-small-canvas-quantity").textContent = smallCanvasOption.value;
+//     cardTemplate.querySelector("#ic-sc-ppu").textContent = `$${scVal}`;
+
+//     cardTemplate.querySelector("#ic-medium-canvas-quantity").textContent = mediumCanvasOption.value;
+//     cardTemplate.querySelector("#ic-mc-ppu").textContent = `$${mcVal}`;
+
+//     cardTemplate.querySelector("#ic-large-canvas-quantity").textContent = largeCanvasOption.value;
+//     cardTemplate.querySelector("#ic-lc-ppu").textContent = `$${lcVal}`;
+
+//     cardTemplate.querySelector("#ic-og-price").textContent;
+//     originalOptionCheck.checked === true ? cardTemplate.querySelector("#og-quantity").textContent = "1" : "0";
+
+//     cardTemplate.querySelector("#ic-small-photo-quantity").textContent = smallPhotoOption.value;
+//     cardTemplate.querySelector("#ic-sp-ppu").textContent = `$${spVal}`;
+
+//     cardTemplate.querySelector("#ic-medium-photo-quantity").textContent = mediumPhotoOption.value;
+//     cardTemplate.querySelector("#ic-mp-ppu").textContent = `$${mpVal}`;
+
+//     cardTemplate.querySelector("#ic-large-photo-quantity").textContent = largePhotoOption.value;
+//     cardTemplate.querySelector("#ic-lp-ppu").textContent = `$${lpVal}`;
+
+//     return cardTemplate;
+// }
+
+
+
+
+// calculate total inventory value
